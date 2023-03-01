4d7542be86f7d2ec5a1e1ac91d2f1964e1374247
Nicholas Massad <nicholasmassad@hotmail.com>
2023-02-28 20:25:23 -0800

    added instructions in readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Project702
+## Instructions
+- To add obstacles, click the right mouse button
+- To add a start point, click the left mouse button
+- To add an end point, click the middle mouse button
+- To clear the screen, press c
+- To clear an obstacle, hover over it with mouse and click x
+- To run the astar_algorithm, press space
--- main.py
+++ main.py
@@ -5,13 +5,6 @@ import game_engine as ge
 import a_star as astar
 import pygame
 
-# To add obstacles, click the right mouse button
-# To add a start point, click the left mouse button
-# To add an end point, click the middle mouse button
-# To clear the screen, press c
-# To clear an obstacle, hover over it with mouse and click x
-# To run the astar_algorithm, press space
-
 # Main function
 def main():
     # Create pygame window and draw grid
